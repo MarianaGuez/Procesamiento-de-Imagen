dcb55d5509eaf3f78ade18af3e7fde7f6c895764
MarianaGuez <marianaguezcas@gmail.com>
2022-08-25 23:37:19 -0500

    agregar imagen de la segunda propuesta

--- Tareas/BarridoSignal_Mejora.m
+++ Tareas/BarridoSignal_Mejora.m
@@ -1,12 +1,11 @@
 imgNegra = zeros(1500,1500);
 
-wDeg = 1;  %size of image (in degrees)
-nPix = 1000;  %resolution of image (pixels);
+wDeg = 1;  %tamaño de imagen(en grados)
+nPix = 1000;  %resolución de la imagen (en pixeles)
 
 [x,y] = meshgrid(linspace(-wDeg/2,wDeg/2,nPix+1));
 
 expo = 0.8*4.5*exp(4.5*x);
-figure(2)
 func = (sin(2*pi*expo).*exp(y*12));
 
 imagesc(func);
